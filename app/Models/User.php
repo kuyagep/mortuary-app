ee4a818d94ab2543ca4d86775deadf23b49edb43
Geraint Lloyd Ortiz <108078862+Kerygma1279@users.noreply.github.com>
2025-08-24 19:46:43 +0800

    fix: migration change to ulid

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::ulid();
+            }
+        });
+    }
     /**
      * The attributes that are mass assignable.
      *
