e425dbe88e68a5b1438d8cddabf57948833df332
Geraint Lloyd Ortiz <dortz062488@gmail.com>
2025-08-24 20:54:37 +0800

    refactor: products

---
+++ app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $guarded = [];
+
+     protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::ulid();
+            }
+        });
+    }
+}
